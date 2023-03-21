7760a566be0e2ee3a73e48861731b4bf8f9819cc
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2023-03-21 15:53:02 -0300

    added SNS usage

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import { S3Handler, S3Event } from "aws-lambda";
+import { S3Event, SNSHandler, SNSEvent } from "aws-lambda";
 import "source-map-support/register";
 import * as AWS from "aws-sdk";
 
@@ -15,7 +15,17 @@ const connectionParam = {
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParam);
 
-export const handler: S3Handler = async (event: S3Event) => {
+export const handler: SNSHandler = async (event: SNSEvent) => {
+  console.log('Processing SNS event', JSON.stringify(event));
+  for(const snsRecord of event.Records) {
+    const s3EventStr = snsRecord.Sns.Message;
+    console.log('Processing S3 event', s3EventStr);
+    const s3Event = JSON.parse(s3EventStr);
+    await processS3Even(s3Event);
+  }
+}
+
+async function processS3Even(event: S3Event) {
   for (const record of event.Records) {
     const key = record.s3.object.key;
     console.log("Processing S3 item with key: ", key);
