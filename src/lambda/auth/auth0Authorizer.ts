1c6fc6304c922ecac56e8a085839d6623a2d4c24
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2023-03-24 13:08:01 -0300

    using middy/secretsManager

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -1,60 +1,59 @@
-import {
-  CustomAuthorizerEvent,
-  CustomAuthorizerResult,
-  CustomAuthorizerHandler,
-} from "aws-lambda";
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from "aws-lambda";
 import "source-map-support/register";
+
 import { verify } from "jsonwebtoken";
 import { JwtToken } from "../../auth/JWTToken";
-import * as AWS from "aws-sdk";
+
+import * as middy from "middy";
+import { secretsManager } from "middy/middlewares";
 
 const secretId = process.env.AUTH_0_SECRET_ID || "";
 const secretField = process.env.AUTH_0_SECRET_FIELD || "";
 
-const client = new AWS.SecretsManager();
-
-// Cache secret if a lambda instance is reused
-let cachedSecret: string;
-
-export const handler: CustomAuthorizerHandler = async (
-  event: CustomAuthorizerEvent,
-  context
-): Promise<CustomAuthorizerResult> => {
-  try {
-    const decodedToken = await verifyToken(event.authorizationToken);
-    console.log("User was authorized!");
-    return {
-      principalId: decodedToken.sub,
-      policyDocument: {
-        Version: "2012-10-17",
-        Statement: [
-          {
-            Action: "execute-api:Invoke",
-            Effect: "Allow",
-            Resource: "*",
-          },
-        ],
-      },
-    };
-  } catch (e) {
-    console.log("User was not authorized", e.message);
-    return {
-      principalId: "user",
-      policyDocument: {
-        Version: "2012-10-17",
-        Statement: [
-          {
-            Action: "execute-api:Invoke",
-            Effect: "Deny",
-            Resource: "*",
-          },
-        ],
-      },
-    };
+export const handler = middy(
+  async (
+    event: CustomAuthorizerEvent,
+    context
+  ): Promise<CustomAuthorizerResult> => {
+    try {
+      const decodedToken = verifyToken(
+        event.authorizationToken,
+        context.AUTH0_SECRET[secretField]
+      );
+      console.log("User was authorized!");
+      return {
+        principalId: decodedToken.sub,
+        policyDocument: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Action: "execute-api:Invoke",
+              Effect: "Allow",
+              Resource: "*",
+            },
+          ],
+        },
+      };
+    } catch (e) {
+      console.log("User was not authorized", e.message);
+      return {
+        principalId: "user",
+        policyDocument: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Action: "execute-api:Invoke",
+              Effect: "Deny",
+              Resource: "*",
+            },
+          ],
+        },
+      };
+    }
   }
-};
+);
 
-async function verifyToken(authHeader: string | undefined): Promise<JwtToken> {
+function verifyToken(authHeader: string | undefined, secret: string): JwtToken {
   if (!authHeader) {
     throw new Error("No Authorization Header.");
   }
@@ -67,29 +66,16 @@ async function verifyToken(authHeader: string | undefined): Promise<JwtToken> {
   const token = split[1];
   console.log("token", token);
 
-  console.log("secretId", secretId);
-  const secret = await getSecret();
-  console.log("secret", secret);
-
   return verify(token, secret) as JwtToken;
 }
 
-async function getSecret() {
-  if (cachedSecret) {
-    return cachedSecret;
-  }
-
-  const secretObject = await client
-    .getSecretValue({ SecretId: secretId })
-    .promise();
-  console.log("secretObject", JSON.stringify(secretObject));
-
-  const SecretsManagerResponse = JSON.parse(secretObject.SecretString);
-
-  console.log("SecretsManagerResponse", JSON.stringify(SecretsManagerResponse));
-  const secret = SecretsManagerResponse.auth0Secret;
-
-  console.log("secret", secret);
-
-  return secret;
-}
+handler.use(
+  secretsManager({
+    cache: true,
+    cacheExpiryInMillis: 60000,
+    throwOnFailedCall: true,
+    secrets: {
+      AUTH0_SECRET: secretId,
+    },
+  })
+);
