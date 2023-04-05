61db0264444943811d9ccc0c0bee1ae4c1846c8e
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2023-04-05 09:11:57 -0300

    RS256 auth start code

---
+++ src/lambda/auth/rs256Auth0Authorizer.ts
@@ -0,0 +1,8 @@
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from "aws-lambda";
+import "source-map-support/register";
+
+export const handler = async (
+  event: CustomAuthorizerEvent
+): Promise<CustomAuthorizerResult> => {
+  return null;
+};
